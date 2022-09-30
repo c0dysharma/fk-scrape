663402f5df08bb6637839d75f8064f5442f360fb
Kuldeep <c0dysharma01@gmail.com>
2022-09-30 23:19:59 +0530

    Added oos, bigImage and category link (experimental)

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const { default: axios } = require("axios");
 const cheerio = require("cheerio");
 
-class Scrapper{
-  async getMeta (link, headers={}){
-    const config = {headers}
+class Scrapper {
+  static baseURI = "https://flipkart.com";
+  async getMeta(link, headers = {}) {
+    const config = { headers };
     const res = await axios.get(link, config);
     const data = await res.data;
     const $ = cheerio.load(data);
@@ -12,7 +13,9 @@ class Scrapper{
     const description = $("._1mXcCf > p");
     const originalPrice = $("._25b18c >  ._3I9_wc");
     const discountedPrice = $("._25b18c > ._16Jk6d");
+    const oos = $('._1dVbu9')
     let productImages = $("._2E1FGS > img");
+    let bigImage = $(".CXW8mj._3nMexc > img");
 
     if (productImages.length) {
       const arr = [];
@@ -20,6 +23,8 @@ class Scrapper{
         arr.push(image.attribs["src"]);
       }
       productImages = arr;
+    } else {
+      if (bigImage.length) productImages = [bigImage[0].attribs["src"]];
     }
 
     const metaData = {
@@ -34,8 +39,24 @@ class Scrapper{
       discountedPrice: discountedPrice.length
         ? discountedPrice[0].firstChild.data
         : null,
+      outOfStock: oos.length ? oos[0].firstChild.data.includes('out of stock') : false
     };
     return metaData;
+  }
+
+  async getProductLinks(link, headers = {}) {
+    const config = { headers };
+    const res = await axios.get(link, config);
+    const data = await res.data;
+    const $ = cheerio.load(data);
+
+    const products = $("._373qXS > ._2UzuFa");
+    const result = [];
+    for (let element of products) {
+      let URI = element.attribs["href"];
+      if (URI) result.push(Scrapper.baseURI + URI);
+    }
+    return result;
   }
 }
 
